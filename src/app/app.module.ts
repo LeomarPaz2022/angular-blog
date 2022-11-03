49ac21369055234d9ed030899509a74cfc11c2ec
LeomarPaz2022 <leomaravpaz@gmail.com>
2022-11-03 14:27:13 -0300

    created component title-hearder and big-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { TitleHeaderComponent } from './components/title-header/title-header.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { RecommendedPostComponent } from './components/recommended-post/recommended-post.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    TitleHeaderComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    RecommendedPostComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/recommended-post/recommended-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recommended-post',
+  templateUrl: './recommended-post.component.html',
+  styleUrls: ['./recommended-post.component.css']
+})
+export class RecommendedPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
